3aedba12137d411be9165fbbc1c9f43a98603c7b
aoko-code <cynthiakhamenge@yahoo.com>
2024-07-01 13:38:18 +0300

    handle caps

--- caesar.py
+++ caesar.py
@@ -1,14 +1,13 @@
 #create a function
-mode = input("Enter the mode(encrypt/ Decrypt): ")
+mode = input("Enter the mode(encrypt/ decrypt): ")
 message = input("Enter the message: ")
 key = int(input("Enter the key(1 - 26): "))
 def caesar_cipher(text, key, mode):
     result = ""
     for char in text:
         if char.isalpha():
-            ascii_offset = 65
+            ascii_offset = 65 if char.isupper() else 97
             if mode == "encrypt":
-
                 shifted = (ord(char) - ascii_offset + key) % 26
             else:
                 shifted = (ord(char) - ascii_offset - key) % 26
@@ -20,6 +19,6 @@ def caesar_cipher(text, key, mode):
 encrypted = caesar_cipher(message, key, 'encrypt')
 print(f"Encrypted: {encrypted}")
 
-decrypted = caesar_cipher(encrypted, key, 'dencrypt')
+decrypted = caesar_cipher(encrypted, key, 'decrypt')
 print(f"Dencrypted: {decrypted}")
 
