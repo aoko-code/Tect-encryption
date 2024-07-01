0df7bef214da86d3b0b39fb4294c8aba342cd554
aoko-code <cynthiakhamenge@yahoo.com>
2024-07-01 13:04:28 +0300

    basic inputs

--- caesar.py
+++ caesar.py
@@ -1,23 +1,25 @@
 #create a function
+mode = input("Enter the mode(encrypt/ Decrypt): ")
 message = input("Enter the message: ")
-def caesar_cipher(text, shift, mode='encrypt'):
+key = int(input("Enter the key(1 - 26): "))
+def caesar_cipher(text, key, mode):
     result = ""
     for char in text:
         if char.isalpha():
             ascii_offset = 65
             if mode == "encrypt":
-                shifted = (ord(char) - ascii_offset + shift) % 26
+
+                shifted = (ord(char) - ascii_offset + key) % 26
             else:
-                shifted = (ord(char) - ascii_offset - shift) % 26
+                shifted = (ord(char) - ascii_offset - key) % 26
             result += chr(shifted + ascii_offset)
         else:
             result += char
     return result
-shift = 3
 
-encrypted = caesar_cipher(message, shift, 'encrypt')
+encrypted = caesar_cipher(message, key, 'encrypt')
 print(f"Encrypted: {encrypted}")
 
-decrypted = caesar_cipher(encrypted, shift, 'dencrypt')
+decrypted = caesar_cipher(encrypted, key, 'dencrypt')
 print(f"Dencrypted: {decrypted}")
 
